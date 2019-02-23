5f095d22f35a0e8ea5749dda92e5feee0cd58c92
Masha-L <lifsh22m@mtholyoke.edu>
2019-02-23 12:23:51 -0500

    databse posting

--- project/js/main.js
+++ project/js/main.js
@@ -9,6 +9,51 @@ function myFunction() {
   $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
 }
 
+  // This gets called whenver the form is submitted (check out the index.ejs).
+  // Uses jQuery to get the message info and passes it to 'addMessage to actually submit the info to firebase.
+  function handleMessageFormSubmit() {
+    var body = $('#new-post-body').val();
+    var title = $('#new-post-title').val();
+
+    addMessage(body, title);
+  }
+
+// This function gets called with the new message information.
+// It gets the user information and uses both to add the post to the database.
+function addMessage(body, title) {
+  var user = firebase.auth().currentUser;
+  var authorPic = user.photoURL;
+  var author = user.displayName;
+
+  var postData = {
+    author: author,
+    authorPic: authorPic,
+    title: title,
+    body: body
+  };
+
+  var newPostKey = firebase.database().ref().child('stream').push().key;
+  firebase.database().ref('/stream/' + newPostKey).set(postData);
+}
+
+// This function sets up a listener- '.on()' gets called automatically whenever something saved in '/stream/' changes.
+// It's main purpose is to iterate over the stream in the database and add each message to the page.
+function initializeStreamListener() {
+  const databaseStreamReference = firebase.database().ref('/stream/');
+
+  databaseStreamReference.on('value', function(snapshot) {
+    var messages = snapshot.val();
+    $('#stream').empty();
+
+    if (messages) {
+      Object.keys(messages).forEach(function (key) {
+        const message = messages[key];
+        $('#stream').append(`<div>${message.body}</div>`);
+      });
+    }
+  });
+}
+
 // The main purpose of this function is to set up a listener (using firebase) for when the auth state changes.
 // If a user isn't authenticated, we should not show the stream and prompt them to log in.
 // If a use IS authenticated, we should load/show the stream and give them the option to log out.
